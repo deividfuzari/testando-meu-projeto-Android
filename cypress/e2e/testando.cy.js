a05a1e66644a36d27cc1550983e08b5ee51b7c16
Deivid Germano Fuzari <deivid.fuzari@smartfit.com>
2024-05-15 13:04:59 -0300

    acessando outra pagina web

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -11,7 +11,7 @@ describe('testando alguns textos ou tags do meu curso já feito', function(){
         cy.get('header').find('h1').should('have.text', 'CURIOSIDADES DE TECNOLOGIA')
     })
 
-    it.only('manipular os elementos do nav', function(){
+    it('manipular os elementos do nav', function(){
         cy.get('nav a[href="#"]').should('have.length', 4)
             .each(function($href){
                 cy.wrap($href).invoke('text', 'Fuzari')
@@ -24,10 +24,16 @@ describe('testando alguns textos ou tags do meu curso já feito', function(){
         cy.get('article').find('h1').should('have.text', 'História do Mascote do Android')
     })
 
-    it.only('pegar uma tag dentro de outra e usar o contains', function(){
+    it('pegar uma tag dentro de outra e usar o contains', function(){
         const nomeTest = 'Deivid'
         cy.contains('p strong', 'Android').invoke('text', nomeTest)
         //cy.get('p').find('strong').contains('Android')
     })
-})
 
+    it.only('acessando uma pagina externa para poder rodar o cypress', function(){
+        //cy.get('a[href="https://androidcommunity.com/dan-morrill-shows-us-the-android-mascot-that-almost-was-20130103/"]')
+        cy.contains('a', 'Inkscape').invoke('removeAttr', 'target').click()
+            //cy.get('a[href="/pt-br/user/register/"]').should('be.visible')
+            cy.contains('p a', 'Registrar').should('be.visible')
+    })
+})
